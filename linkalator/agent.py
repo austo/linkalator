dcfcf3b9d5cca0068a900d0351f51422c4cff6d7
Austin Moran <austin@queequeg.(none)>
2012-12-03 18:38:30 -0500

    end of (work)day checkin - links are working with id's -> need to make ajax call.

---
+++ linkalator/agent.py
@@ -0,0 +1,6 @@
+class Agent:
+	def __init__(self, id = 0, name = "", city = "", commission = 0.0):
+		self.id = id
+		self.name = name
+		self.city = city
+		self.commission = commission	
---
+++ runserver.py
@@ -0,0 +1,3 @@
+#import pdb; pdb.set_trace()
+from linkalator import app
+app.run()
