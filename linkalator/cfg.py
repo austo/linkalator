d525d1f264a08b624d9391a5500c62f0f9ac1370
Austin Moran <austin@queequeg.(none)>
2012-12-03 15:58:55 -0500

    initial

---
+++ linkalator/cfg.py
@@ -0,0 +1,5 @@
+class Cfg:
+    def __init__(self):
+        self.connection_string = 'austin/Hell0Sql@queequeg/xe'
+        self.app_user_connection_string = 'linkalator/hEracl3s@queequeg/xe'
+        self.log_detail = "Request url: {!s}; Method: {!s}; Returned status code: {!s}"
